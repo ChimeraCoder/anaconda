679a4b88f1a5e0e2622fc83f807556d13e7010cf
Aditya Mukerjee <aditya@quotidianventures.com>
2013-02-25 23:13:36 +0000

    Refactor API queries to be methods of a TwitterApi struct

--- twitter.go
+++ twitter.go
@@ -16,14 +16,19 @@ var OauthClient = oauth.Client{
 	TokenRequestURI:               "http://api.twitter.com/oauth/access_token",
 }
 
+type TwitterApi struct{
+    Credentials *oauth.Credentials
+}
+
+
 func SetCredentials(client_marshalled string) (err error) {
 	err = json.Unmarshal([]byte(client_marshalled), &OauthClient)
 	return
 }
 
 // apiGet issues a GET request to the Twitter API and decodes the response JSON to data.
-func apiGet(cred *oauth.Credentials, urlStr string, form url.Values, data interface{}) error {
-	resp, err := OauthClient.Get(http.DefaultClient, cred, urlStr, form)
+func (c TwitterApi) apiGet(urlStr string, form url.Values, data interface{}) error {
+	resp, err := OauthClient.Get(http.DefaultClient, c.Credentials, urlStr, form)
 	if err != nil {
 		return err
 	}
@@ -51,31 +56,31 @@ func decodeResponse(resp *http.Response, data interface{}) error {
 	return json.NewDecoder(resp.Body).Decode(data)
 }
 
-func GetHomeTimeline(client *oauth.Credentials) (timeline []Tweet, err error) {
+func (a TwitterApi) GetHomeTimeline() (timeline []Tweet, err error) {
 	v := url.Values{}
 	v.Set("include_entities", "true")
-	err = apiGet(client, "http://api.twitter.com/1.1/statuses/home_timeline.json", v, &timeline)
+	err = a.apiGet("http://api.twitter.com/1.1/statuses/home_timeline.json", v, &timeline)
 	return
 }
 
-func GetUserTimeline(client *oauth.Credentials, v url.Values) (timeline []Tweet, err error) {
-	err = apiGet(client, "http://api.twitter.com/1.1/statuses/user_timeline.json", v, &timeline)
+func (a TwitterApi) GetUserTimeline(v url.Values) (timeline []Tweet, err error) {
+	err = a.apiGet("http://api.twitter.com/1.1/statuses/user_timeline.json", v, &timeline)
 	return
 }
 
-func GetMentionsTimeline(client *oauth.Credentials, v url.Values) (timeline []Tweet, err error) {
-	err = apiGet(client, "http://api.twitter.com/1.1/statuses/mentions_timeline.json", v, &timeline)
+func (a TwitterApi) GetMentionsTimeline(v url.Values) (timeline []Tweet, err error) {
+	err = a.apiGet("http://api.twitter.com/1.1/statuses/mentions_timeline.json", v, &timeline)
     return
 }
 
-func GetRetweetsOfMe(client *oauth.Credentials, v url.Values) (tweets []Tweet, err error) {
-    err = apiGet(client, "https://api.twitter.com/1.1/statuses/retweets_of_me.json", v, &tweets)
+func (a TwitterApi) GetRetweetsOfMe(v url.Values) (tweets []Tweet, err error) {
+    err = a.apiGet("https://api.twitter.com/1.1/statuses/retweets_of_me.json", v, &tweets)
     return
 }
 
-func GetTweet(client *oauth.Credentialis, id int64, v url.Values) (tweet Tweet, err error){
+func (a TwitterApi) GetTweet(id int64, v url.Values) (tweet Tweet, err error){
     v.Set("id", strconv.FormatInt(id,10))
-    err = apiGet(client, "https://api.twitter.com/1.1/statuses/show.json", v, &tweet)
+    err = a.apiGet("https://api.twitter.com/1.1/statuses/show.json", v, &tweet)
     return
 }
 
