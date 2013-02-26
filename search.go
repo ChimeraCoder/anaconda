b32ec7f25d6867b5a1f72cf89759f5a1a25733b6
Aditya Mukerjee <aditya@quotidianventures.com>
2013-02-26 17:09:01 +0000

    Implement GET /search/tweets

---
+++ search.go
@@ -0,0 +1,17 @@
+package twitter
+
+import (
+    "net/url"
+)
+
+type searchResponse struct {
+    Statuses []Tweet
+}
+
+func (a TwitterApi) GetSearch(query string, v url.Values) (timeline []Tweet, err error) {
+    var sr searchResponse
+    v.Set("q", query)
+    err = a.apiGet("https://api.twitter.com/1.1/search/tweets.json", v, &sr)
+    timeline = sr.Statuses
+	return
+}
