2ec074a88f2ba4ca06fee53419ba36fcf56dae45
aditya <dev@chimeracoder.net>
2013-03-05 21:31:38 -0500

    Fix package name in twitter_test.go (fixes issue #3)

--- twitter_test.go
+++ twitter_test.go
@@ -1,15 +1,19 @@
 package anaconda
 
 import (
+    "github.com/ChimeraCoder/anaconda"
 	"fmt"
 )
 
 func ExampleSearch() {
 
-	twitter.SetConsumerKey("your-consumer-key")
-	twitter.SetConsumerSecret("your-consumer-secret")
-	api := twitter.NewTwitterApi("your-access-token", "your-access-token-secret")
-	search_result, err := api.GetSearch("golang", url.Values{})
+	anaconda.SetConsumerKey("your-consumer-key")
+	anaconda.SetConsumerSecret("your-consumer-secret")
+	api := anaconda.NewTwitterApi("your-access-token", "your-access-token-secret")
+	search_result, err := api.GetSearch("golang", nil)
+    if err != nil{
+        panic(err)
+    }
 	for _, tweet := range search_result {
 		fmt.Print(tweet.Text)
 	}
