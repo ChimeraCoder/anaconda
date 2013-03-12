eefa583babe7327ba4447a4dc620e66e6fcaa543
Aditya Mukerjee <aditya@quotidianventures.com>
2013-03-12 19:03:09 +0000

    Add an example and a test for Twitter error handling
    * This tests an error that originates with Twitter, as opposed to other
    errors

--- twitter_test.go
+++ twitter_test.go
@@ -3,6 +3,7 @@ package anaconda_test
 import (
 	"fmt"
 	"github.com/ChimeraCoder/anaconda"
+	"testing"
 )
 
 func ExampleTwitterApi_GetSearch() {
@@ -17,4 +18,28 @@ func ExampleTwitterApi_GetSearch() {
 	for _, tweet := range search_result {
 		fmt.Print(tweet.Text)
 	}
+}
+
+func ExampleErrorHandling() {
+	api := anaconda.NewTwitterApi("invalid", "token")
+	_, err := api.GetSearch("golang", nil)
+	if err != nil {
+		twitter_error := err.(anaconda.ApiError).TwitterErrors.(anaconda.TwitterError)
+		if twitter_error.ContainsError(anaconda.TwitterErrorBadAuthenticationData) {
+			//Some logic that fixes the authentication data
+		}
+	}
+}
+
+func TestTwitterErrorBadAuthenticationData(t *testing.T) {
+	api := anaconda.NewTwitterApi("invalid", "token")
+	_, err := api.GetSearch("golang", nil)
+	if err != nil {
+		aerr := err.(anaconda.ApiError)
+		desired_error := anaconda.TwitterErrorBadAuthenticationData
+		if !aerr.TwitterErrors.(anaconda.TwitterError).ContainsError(desired_error) {
+			t.Errorf("Expected error code %d and received %+v", desired_error, aerr)
+		}
+	}
+
 }
