6537e488ef21b5ef81b700c8c7ef2a1983883ffa
Brian Hamman <hamman@gmail.com>
2013-08-09 16:03:00 -0400

    added basic user lookup search

---
+++ users.go
@@ -0,0 +1,12 @@
+package anaconda
+
+import (
+	"net/url"
+)
+
+func (a TwitterApi) GetUsersLookup(usernames string, v url.Values) (u []TwitterUser, err error) {
+	v = cleanValues(v)
+	v.Set("screen_name", usernames)
+	err = a.apiGet("http://api.twitter.com/1.1/users/lookup.json", v, &u)
+	return
+}
