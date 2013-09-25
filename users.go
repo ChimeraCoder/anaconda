3cf61438069d4332ab17d86a4d7a433517c336b2
aditya <dev@chimeracoder.net>
2013-09-25 19:15:13 -0400

    Implement rate limiting / throttling for GetUsersLookup

--- users.go
+++ users.go
@@ -8,8 +8,11 @@ import (
 func (a TwitterApi) GetUsersLookup(usernames string, v url.Values) (u []TwitterUser, err error) {
 	v = cleanValues(v)
 	v.Set("screen_name", usernames)
-	err = a.apiGet("http://api.twitter.com/1.1/users/lookup.json", v, &u)
-	return
+	//err = a.apiGet("http://api.twitter.com/1.1/users/lookup.json", v, &u)
+	response_ch := make(chan response)
+	a.queryQueue <- query{"http://api.twitter.com/1.1/users/lookup.json", v, &u, _GET, response_ch}
+	response := <-response_ch
+	return u, response.err
 }
 
 func (a TwitterApi) GetUsersLookupByIds(ids []int64, v url.Values) (u []TwitterUser, err error) {
