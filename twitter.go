6dad8cc14d2d6f68a543e7cdaa0b92c35cc01eef
Aditya Mukerjee <dev@chimeracoder.net>
2013-09-27 11:44:36 -0400

    Fix delay to subtract out the time that the request took
    * If a request took 10 seconds, we don't need to wait for another 10
    seconds

--- twitter.go
+++ twitter.go
@@ -228,6 +228,7 @@ func (c TwitterApi) execQuery(urlStr string, form url.Values, data interface{}, 
 //throttledQuery executes queries and automatically throttles them according to SECONDS_PER_QUERY
 func (c TwitterApi) throttledQuery() {
 	for q := range c.queryQueue {
+        now := time.Now()
 		url := q.url
 		form := q.form
 		data := q.data //This is where the actual response will be written
@@ -245,6 +246,6 @@ func (c TwitterApi) throttledQuery() {
 		c.delay_mutex.Lock()
 		delay := c.delay
 		c.delay_mutex.Unlock()
-		time.Sleep(delay)
+		time.Sleep(delay - time.Since(now))
 	}
 }
