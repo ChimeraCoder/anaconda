32df1fb11e42686da05877279fcf6a6bf161123c
Aditya Mukerjee <dev@chimeracoder.net>
2013-10-06 12:53:24 -0400

    Add struct that will be used to handle Twitter's errors

---
+++ errors.go
@@ -0,0 +1,44 @@
+package anaconda
+
+const (
+	//Error code defintions match the Twitter documentation
+	//https://dev.twitter.com/docs/error-codes-responses
+	TwitterErrorCouldNotAuthenticate    = 32
+	TwitterErrorDoesNotExist            = 34
+	TwitterErrorAccountSuspended        = 64
+	TwitterErrorApi1Deprecation         = 68 //This should never be needed
+	TwitterErrorRateLimitExceeded       = 88
+	TwitterErrorInvalidToken            = 89
+	TwitterErrorOverCapacity            = 130
+	TwitterErrorInternalError           = 131
+	TwitterErrorCouldNotAuthenticateYou = 135
+	TwitterErrorStatusIsADuplicate      = 187
+	TwitterErrorBadAuthenticationData   = 215
+	TwitterErrorUserMustVerifyLogin     = 231
+)
+
+//TwitterErrorResponse has an array of Twitter error messages
+//It satisfies the "error" interface
+//For the most part, Twitter seems to return only a single error message
+//Currently, we assume that this always contains exactly one error message
+type TwitterErrorResponse struct {
+	errors []TwitterError
+}
+
+func (tr TwitterErrorResponse) First() error {
+	return tr.errors[0]
+}
+
+func (tr TwitterErrorResponse) Error() string {
+	return tr.errors[0].Message
+}
+
+//TwitterError represents a single Twitter error messages/code pair
+type TwitterError struct {
+	Message string
+	Code    int
+}
+
+func (te TwitterError) Error() string {
+	return te.Message
+}
