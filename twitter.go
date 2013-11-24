0245a9294c4dd5171bf44b3803074299c69b896a
aditya <dev@chimeracoder.net>
2013-11-23 22:05:20 -0500

    Change default delay to 0 seconds (ie, rate-limiting is off by default)

--- twitter.go
+++ twitter.go
@@ -81,7 +81,7 @@ type response struct {
 	err  error
 }
 
-const DEFAULT_DELAY = 10 * time.Second
+const DEFAULT_DELAY = 0 * time.Second
 
 //NewTwitterApi takes an user-specific access token and secret and returns a TwitterApi struct for that user.
 //The TwitterApi struct can be used for accessing any of the endpoints available.
