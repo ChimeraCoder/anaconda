0e824c5d8b2424b06a2ee4f1b4ad65d12a88a4cf
Jon Eisen <jon@metricfeat.com>
2013-11-25 20:08:46 -0500

    Add GetOEmbed
    Signed-off-by: aditya <dev@chimeracoder.net>

---
+++ oembed.go
@@ -0,0 +1,32 @@
+package anaconda
+
+import (
+	"net/url"
+	"net/http"
+)
+
+type OEmbed struct {
+	Type string
+	Width int
+	Cache_age string
+	Height int
+	Author_url string
+	Html string
+	Version string
+	Provider_name string
+	Provider_url string
+	Url string
+	Author_name string
+}
+
+// No authorization on this endpoint. Its the only one.
+func (a TwitterApi) GetOEmbed(v url.Values) (o OEmbed, err error) {
+	resp, err := http.Get("http://api.twitter.com/1/statuses/oembed.json?" + v.Encode())
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	err = decodeResponse(resp, &o)
+	return
+}
