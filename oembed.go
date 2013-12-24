52e9221a2f3a4e12f064e4edb0401d9bbb7ad154
aditya <dev@chimeracoder.net>
2013-12-24 12:30:29 -0500

    Add GetOEmbedId (fetch an OEmbed by specifying the tweet Id)

--- oembed.go
+++ oembed.go
@@ -3,6 +3,7 @@ package anaconda
 import (
 	"net/http"
 	"net/url"
+	"strconv"
 )
 
 type OEmbed struct {
@@ -21,6 +22,22 @@ type OEmbed struct {
 
 // No authorization on this endpoint. Its the only one.
 func (a TwitterApi) GetOEmbed(v url.Values) (o OEmbed, err error) {
+	resp, err := http.Get("http://api.twitter.com/1/statuses/oembed.json?" + v.Encode())
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	err = decodeResponse(resp, &o)
+	return
+}
+
+// Calls GetOEmbed with the corresponding id. Convenience wrapper for GetOEmbed()
+func (a TwitterApi) GetOEmbedId(id int64, v url.Values) (o OEmbed, err error) {
+	if v == nil {
+		v = url.Values{}
+	}
+	v.Set("id", strconv.FormatInt(id, 10))
 	resp, err := http.Get("http://api.twitter.com/1/statuses/oembed.json?" + v.Encode())
 	if err != nil {
 		return
