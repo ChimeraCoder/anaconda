540ec03b6355f7f91d4a426754acbb35fb77505e
Juuso Haavisto <juuso@mail.com>
2014-02-27 12:44:42 +0200

    Attempt to add direct message posting

--- directmessages.go
+++ directmessages.go
@@ -21,3 +21,11 @@ func (a TwitterApi) GetDirectMessagesShow(v url.Values) (messages []DirectMessag
 	a.queryQueue <- query{BaseUrl + "/direct_messages/show.json", v, &messages, _GET, response_ch}
 	return messages, (<-response_ch).err
 }
+
+func (a TwitterApi) PostDirectMessage(status string, v url.Values) (tweet Tweet, err error) {
+	v = cleanValues(v)
+	v.Set("status", status)
+	response_ch := make(chan response)
+	a.queryQueue <- query{BaseUrl + "/direct_messages/new.json", v, &tweet, _POST, response_ch}
+	return tweet, (<-response_ch).err
+}
