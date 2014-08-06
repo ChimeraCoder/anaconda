5bb53ba854a8a9d393e18a25c81365597eb7ae85
Paolo Ibarra <jpibarra1130@gmail.com>
2014-08-06 14:23:22 +0800

    Added ability to send direct message.

--- directmessages.go
+++ directmessages.go
@@ -21,3 +21,9 @@ func (a TwitterApi) GetDirectMessagesShow(v url.Values) (messages []DirectMessag
 	a.queryQueue <- query{BaseUrl + "/direct_messages/show.json", v, &messages, _GET, response_ch}
 	return messages, (<-response_ch).err
 }
+
+func (a TwitterApi) SendDirectMessage(v url.Values) (message DirectMessage, err error) {
+	response_ch := make(chan response)
+	a.queryQueue <- query{BaseUrl + "/direct_messages/new.json", v, &message, _POST, response_ch}
+	return message, (<-response_ch).err
+}
