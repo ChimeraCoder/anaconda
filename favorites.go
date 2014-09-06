deaffaf4e9a4b7279110b4955e1b1cbc5966b441
mchmarny <mark@chmarny.com>
2014-09-06 10:02:46 -0700

    Added favorite delete implementation

--- favorites.go
+++ favorites.go
@@ -9,3 +9,11 @@ func (a TwitterApi) GetFavorites(v url.Values) (favorites []Tweet, err error) {
 	a.queryQueue <- query{BaseUrl + "/favorites/list.json", v, &favorites, _GET, response_ch}
 	return favorites, (<-response_ch).err
 }
+
+func (a TwitterApi) DeleteFavorite(id int64) error {
+	v := url.Values{}
+	v.Set("id", string(id))
+	response_ch := make(chan response)
+	a.queryQueue <- query{BaseUrl + "favorites/destroy.json", v, nil, _POST, response_ch}
+	return (<-response_ch).err
+}
