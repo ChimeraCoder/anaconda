d57da725a6296f35a2d1ec3c86e843f97f8b8ab7
mchmarny <mark@chmarny.com>
2014-09-06 16:14:59 -0700

    Fixed the null tweet fix

--- favorites.go
+++ favorites.go
@@ -11,9 +11,7 @@ func (a TwitterApi) GetFavorites(v url.Values) (favorites []Tweet, err error) {
 }
 
 func (a TwitterApi) DeleteFavorite(favorite Tweet) (t Tweet, err error) {
-	v := url.Values{}
-	v.Set("id", string(favorite.Id))
 	response_ch := make(chan response)
-	a.queryQueue <- query{BaseUrl + "/favorites/destroy.json", v, &favorite, _POST, response_ch}
+	a.queryQueue <- query{BaseUrl + "/favorites/destroy.json?id=" + favorite.IdStr, nil, &favorite, _POST, response_ch}
 	return favorite, (<-response_ch).err
 }
