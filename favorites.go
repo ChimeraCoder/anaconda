81fd1e251c131854a228b8c6758654fbeda92705
mchmarny <mark@chmarny.com>
2014-09-07 13:40:44 -0700

    Added larger page defaults

--- favorites.go
+++ favorites.go
@@ -10,8 +10,8 @@ func (a TwitterApi) GetFavorites(v url.Values) (favorites []Tweet, err error) {
 	return favorites, (<-response_ch).err
 }
 
-func (a TwitterApi) DeleteFavorite(favorite Tweet, v url.Values) (t Tweet, err error) {
+func (a TwitterApi) DeleteFavorite(favorite Tweet) (t Tweet, err error) {
 	response_ch := make(chan response)
-	a.queryQueue <- query{BaseUrl + "/favorites/destroy.json?id=" + favorite.IdStr, v, &favorite, _POST, response_ch}
+	a.queryQueue <- query{BaseUrl + "/favorites/destroy.json?id=" + favorite.IdStr, nil, &favorite, _POST, response_ch}
 	return favorite, (<-response_ch).err
 }
