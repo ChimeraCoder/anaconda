8efa3165ec810a8d29511ada453fffb3d0d45a37
Kendrick Martin <kmartin@crowdcompass.com>
2014-11-23 16:22:14 -0800

    Adding GetStatusesUserTimeline

--- statuses.go
+++ statuses.go
@@ -15,3 +15,11 @@ func (a TwitterApi) GetStatusesMentionsTimeline(v url.Values) (timeline []Tweet,
 	a.queryQueue <- query{BaseUrl + "/statuses/mentions_timeline.json", v, &timeline, _GET, response_ch}
 	return timeline, (<-response_ch).err
 }
+
+func (a TwitterApi) GetStatusesUserTimeline(v url.Values) (timeline []Tweet, err error) {
+	v = cleanValues(v)
+
+	response_ch := make(chan response)
+	a.queryQueue <- query{BaseUrl + "/statuses/user_timeline.json", v, &timeline, _GET, response_ch}
+	return timeline, (<-response_ch).err
+}
