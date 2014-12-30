edc11b013d35938d0baf536554d0a7ff659ef724
aditya <dev@chimeracoder.net>
2014-12-29 23:35:08 -0500

    Add Coordinates struct

--- tweet.go
+++ tweet.go
@@ -5,29 +5,34 @@ import (
 )
 
 type Tweet struct {
-	Contributors         []int64     `json:"contributors"`
-	Coordinates          interface{} `json:"coordinates"`
-	CreatedAt            string      `json:"created_at"`
-	Entities             Entities    `json:"entities"`
-	FavoriteCount        int         `json:"favorite_count"`
-	Favorited            bool        `json:"favorited"`
-	Geo                  interface{} `json:"geo"`
-	Id                   int64       `json:"id"`
-	IdStr                string      `json:"id_str"`
-	InReplyToScreenName  string      `json:"in_reply_to_screen_name"`
-	InReplyToStatusID    int64       `json:"in_reply_to_status_id"`
-	InReplyToStatusIdStr string      `json:"in_reply_to_status_id_str"`
-	InReplyToUserID      int64       `json:"in_reply_to_user_id"`
-	InReplyToUserIdStr   string      `json:"in_reply_to_user_id_str"`
-	Place                Place       `json:"place"`
-	PossiblySensitive    bool        `json:"possibly_sensitive"`
-	RetweetCount         int         `json:"retweet_count"`
-	Retweeted            bool        `json:"retweeted"`
-	RetweetedStatus      *Tweet      `json:"retweeted_status"`
-	Source               string      `json:"source"`
-	Text                 string      `json:"text"`
-	Truncated            bool        `json:"truncated"`
-	User                 User        `json:"user"`
+	Contributors         []int64      `json:"contributors"`
+	Coordinates          *Coordinates `json:"coordinates"`
+	CreatedAt            string       `json:"created_at"`
+	Entities             Entities     `json:"entities"`
+	FavoriteCount        int          `json:"favorite_count"`
+	Favorited            bool         `json:"favorited"`
+	Geo                  interface{}  `json:"geo"`
+	Id                   int64        `json:"id"`
+	IdStr                string       `json:"id_str"`
+	InReplyToScreenName  string       `json:"in_reply_to_screen_name"`
+	InReplyToStatusID    int64        `json:"in_reply_to_status_id"`
+	InReplyToStatusIdStr string       `json:"in_reply_to_status_id_str"`
+	InReplyToUserID      int64        `json:"in_reply_to_user_id"`
+	InReplyToUserIdStr   string       `json:"in_reply_to_user_id_str"`
+	Place                Place        `json:"place"`
+	PossiblySensitive    bool         `json:"possibly_sensitive"`
+	RetweetCount         int          `json:"retweet_count"`
+	Retweeted            bool         `json:"retweeted"`
+	RetweetedStatus      *Tweet       `json:"retweeted_status"`
+	Source               string       `json:"source"`
+	Text                 string       `json:"text"`
+	Truncated            bool         `json:"truncated"`
+	User                 User         `json:"user"`
+}
+
+type Coordinates struct {
+	Coordinates []float64 `json:"coordinates"`
+	Type        string    `json:"type"`
 }
 
 // CreatedAtTime is a convenience wrapper that returns the Created_at time, parsed as a time.Time struct
