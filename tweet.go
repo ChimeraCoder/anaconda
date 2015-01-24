e084a70f99e02e63fa2c27a6853274e003b3507b
Erik Seglem <erik.seglem@gmail.com>
2015-01-23 23:26:07 -0500

    Clean up Coordinate helpers, Adjust Contributors
    Signed-off-by: aditya <dev@chimeracoder.net>

--- tweet.go
+++ tweet.go
@@ -5,11 +5,8 @@ import (
 )
 
 type Tweet struct {
-	Contributors []User `json:"contributors"`
-	Coordinates  struct {
-		Coordinates []float64 `json:"coordinates"`
-		Type        string     `json:"type"`
-	} `json:"coordinates"`
+	Contributors         []Contributor     `json:"contributors"` // Not yet generally available to all, so hard to test
+	Coordinates          *Coordinates      `json:"coordinates"`
 	CreatedAt            string      `json:"created_at"`
 	Entities             Entities    `json:"entities"`
 	FavoriteCount        int         `json:"favorite_count"`
@@ -36,7 +33,8 @@ type Tweet struct {
 	WithheldCopyright    bool        `json:"withheld_copyright"`
 	WithheldInCountries  []string    `json:"withheld_in_countries"`
 	WithheldScope        string      `json:"withheld_scope"`
-	//Geo is deprecated, discourage usage
+
+	//Geo is deprecated
 	//Geo                  interface{} `json:"geo"`
 }
 
@@ -45,25 +43,52 @@ func (t Tweet) CreatedAtTime() (time.Time, error) {
 	return time.Parse(time.RubyDate, t.CreatedAt)
 }
 
+// It may be worth placing these in an additional source file(s)
+
+// Could also use User, since the fields match, but only these fields are possible in Contributor
+type Contributor struct {
+	Id         int64  `json:"id"`
+	IdStr      string `json:"id_str"`
+	ScreenName string `json:"screen_name"`
+}
+
+type Coordinates struct {
+	Coordinates [2]float64 `json:"coordinates"` // Coordinate always has to have exactly 2 values
+	Type        string     `json:"type"`
+}
+
+// HasCoordinates is a helper function to easily determine if a Tweet has coordinates associated with it
 func (t Tweet) HasCoordinates() bool {
-	if t.Coordinates.Type == "Point" {
+	if t.Coordinates != nil {
 		return true
 	}
 	return false
 }
 
-// Latitude is a convenience wrapper that returns the latitude easily, not sure the best default return vaules
-func (t Tweet) Latitude() float64 {
+// The following provide convenience and eliviate confusion about the order of coordinates in the Tweet
+
+// Latitude is a convenience wrapper that returns the latitude easily
+func (t Tweet) Latitude() (float64, error) {
 	if t.HasCoordinates() {
-		return t.Coordinates.Coordinates[1]
+		return t.Coordinates.Coordinates[1], nil
 	}
-	return -9999
+	return 0, errors.New("No Coordinates in this Tweet")
 }
 
-// Longitude is a convenience wrapper that returns the longitude easily, not sure the best default return values
-func (t Tweet) Longitude() float64 {
+// Longitude is a convenience wrapper that returns the longitude easily
+func (t Tweet) Longitude() (float64, error) {
 	if t.HasCoordinates() {
-		return t.Coordinates.Coordinates[0]
+		return t.Coordinates.Coordinates[0], nil
 	}
-	return -9999
+	return 0, errors.New("No Coordinates in this Tweet")
+}
+
+// X is a concenience wrapper which returns the X (Longitude) coordinate easily
+func (t Tweet) X() (float64, error) {
+	return t.Longitude()
+}
+
+// Y is a convenience wrapper which return the Y (Lattitude) corrdinate easily
+func (t Tweet) Y() (float64, error) {
+	return t.Latitude()
 }
