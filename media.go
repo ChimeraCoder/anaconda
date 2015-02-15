14a2c3021c20df797e029b5f1fe26ef3dfe6f596
Kenta Shoji <shoji@emosiv.com>
2015-02-15 14:50:01 +0900

    Add media
    Signed-off-by: aditya <dev@chimeracoder.net>

---
+++ media.go
@@ -0,0 +1,27 @@
+package anaconda
+
+import "net/url"
+
+type Media struct {
+	MediaID       int64  `json:"media_id"`
+	MediaIDString string `json:"media_id_string"`
+	Size          int    `json:"size"`
+	Image         Image  `json:"image"`
+}
+
+type Image struct {
+	W         int    `json:"w"`
+	H         int    `json:"h"`
+	ImageType string `json:"image_type"`
+}
+
+func (a TwitterApi) UploadMedia(base64String string) (media Media, err error) {
+	v := url.Values{}
+	v.Set("media", base64String)
+
+	var mediaResponse Media
+
+	response_ch := make(chan response)
+	a.queryQueue <- query{BaseUrl + "/media/upload.json", v, &mediaResponse, _POST, response_ch}
+	return mediaResponse, (<-response_ch).err
+}
