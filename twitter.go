28b80e5e8108543f4ff6f6886d6f9f0650fb52c2
Kenta Shoji <shoji@emosiv.com>
2015-02-15 15:04:32 +0900

    Add UploadBaseUrl

--- twitter.go
+++ twitter.go
@@ -52,10 +52,11 @@ import (
 )
 
 const (
-	_GET      = iota
-	_POST     = iota
-	BaseUrlV1 = "https://api.twitter.com/1"
-	BaseUrl   = "https://api.twitter.com/1.1"
+	_GET          = iota
+	_POST         = iota
+	BaseUrlV1     = "https://api.twitter.com/1"
+	BaseUrl       = "https://api.twitter.com/1.1"
+	UploadBaseUrl = "https://upload.twitter.com/1.1"
 )
 
 var oauthClient = oauth.Client{
