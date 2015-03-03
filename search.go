824299cb210ce27f1f9003032ec65196365dc256
Jason Buberel <jason@buberel.org>
2015-03-02 17:36:19 -0800

    Adding support for the GetNext() mehthod

--- search.go
+++ search.go
@@ -2,6 +2,7 @@ package anaconda
 
 import (
 	"net/url"
+	"log"
 )
 
 type SearchMetadata struct {
@@ -21,10 +22,35 @@ type SearchResponse struct {
 	Metadata SearchMetadata `json:"search_metadata"`
 }
 
+func (sr *SearchResponse) GetNext(a *TwitterApi)  (SearchResponse, error) {
+	log.Printf("GetNext invoked\n")
+	if sr.Metadata.NextResults == "" {
+		return SearchResponse{}, nil
+	}
+	nextUrl, err := url.Parse(sr.Metadata.NextResults)
+	if  err != nil {
+		return SearchResponse{}, err
+	}
+
+	v := nextUrl.Query()
+	log.Printf("GetNext values: %v\n", v)
+	// remove the q parameter from the url.Values so that it
+	// can be added back via the next GetSearch method call.
+	delete(v, "q")
+
+	q, _ := url.QueryUnescape(sr.Metadata.Query)
+	if err != nil {
+		return SearchResponse{}, err
+	}
+	log.Printf("Performing next search with query: %v\n", q)
+	newSr, err := a.GetSearch(q, v)
+	return newSr, err
+}
+
 func (a TwitterApi) GetSearch(queryString string, v url.Values) (sr SearchResponse, err error) {
 	v = cleanValues(v)
 	v.Set("q", queryString)
-
+	log.Printf("Search query:[%v]\n", v)
 	response_ch := make(chan response)
 	a.queryQueue <- query{BaseUrl + "/search/tweets.json", v, &sr, _GET, response_ch}
 
