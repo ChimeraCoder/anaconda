cb7f2b7dec8997127876001be233403076a6944c
Alex Logashov <admin@shiftarray.com>
2016-01-03 20:24:21 -0800

    Fix Entities serialization

--- twitter_entities.go
+++ twitter_entities.go
@@ -1,63 +1,59 @@
 package anaconda
 
-type UrlEntity struct {
-	Urls []struct {
-		Indices      []int
-		Url          string
-		Display_url  string
-		Expanded_url string
-	}
+type Hashtags struct {
+	Indices []int  `json:"indices"`
+	Text    string `json:"text"`
+}
+
+type Urls struct {
+	Indices     []int  `json:"indices"`
+	Url         string `json:"url"`
+	DisplayUrl  string `json:"display_url"`
+	ExpandedUrl string `json:"expanded_url"`
+}
+
+type UserMentions struct {
+	Name       string `json:"name"`
+	Indices    []int  `json:"indices"`
+	ScreenName string `json:"screen_name"`
+	Id         int64  `json:"id"`
+	Id_str     string `json:"id_str"`
 }
 
 type Entities struct {
-	Hashtags []struct {
-		Indices []int
-		Text    string
-	}
-	Urls []struct {
-		Indices      []int
-		Url          string
-		Display_url  string
-		Expanded_url string
-	}
-	Url           UrlEntity
-	User_mentions []struct {
-		Name        string
-		Indices     []int
-		Screen_name string
-		Id          int64
-		Id_str      string
-	}
-	Media []EntityMedia
+	Hashtags     []Hashtags     `json:"hashtags"`
+	Urls         []Urls         `json:"urls"`
+	Media        []EntityMedia  `json:"media"`
+	UserMentions []UserMentions `json:"user_mentions"`
 }
 
 type EntityMedia struct {
-	Id                   int64
-	Id_str               string
-	Media_url            string
-	Media_url_https      string
-	Url                  string
-	Display_url          string
-	Expanded_url         string
-	Sizes                MediaSizes
-	Source_status_id     int64
-	Source_status_id_str string
-	Type                 string
-	Indices              []int
-	VideoInfo            VideoInfo `json:"video_info"`
+	Id                int64      `json:"id"`
+	Id_str            string     `json:"id_str"`
+	MediaUrl          string     `json:"media_url"`
+	MediaUrlHttps     string     `json:"media_url_https"`
+	Url               string     `json:"url"`
+	DisplayUrl        string     `json:"display_url"`
+	ExpandedUrl       string     `json:"expanded_url"`
+	Sizes             MediaSizes `json:"sizes"`
+	SourceStatusId    int64      `json:"source_status_id"`
+	SourceStatusIdStr string     `json:"source_status_id_str"`
+	Type              string     `json:"type"`
+	Indices           []int      `json:"indices"`
+	VideoInfo         VideoInfo  `json:"video_info"`
 }
 
 type MediaSizes struct {
-	Medium MediaSize
-	Thumb  MediaSize
-	Small  MediaSize
-	Large  MediaSize
+	Medium MediaSize `json:"medium"`
+	Thumb  MediaSize `json:"thumb"`
+	Small  MediaSize `json:"small"`
+	Large  MediaSize `json:"large"`
 }
 
 type MediaSize struct {
-	W      int
-	H      int
-	Resize string
+	W      int    `json:"w"`
+	H      int    `json:"h"`
+	Resize string `json:"resize"`
 }
 
 type VideoInfo struct {
