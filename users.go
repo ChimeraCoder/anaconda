1c6657c679b8c247c7464c89283a3e3f69113f92
andefined <dimitris.papaevagelou@andefined.com>
2017-08-28 14:00:50 +0300

    Add Feature / report_spam

--- users.go
+++ users.go
@@ -54,3 +54,27 @@ func (a TwitterApi) GetUserSearch(searchTerm string, v url.Values) (u []User, er
 	a.queryQueue <- query{a.baseUrl + "/users/search.json", v, &u, _GET, response_ch}
 	return u, (<-response_ch).err
 }
+
+// PostUsersReportAsSpam : Reports and Blocks a User by screen_name
+// Reference : https://dev.twitter.com/rest/reference/post/users/report_spam
+// If you don't want to block the user you should add
+// v.Set("perform_block", "false")
+func (a TwitterApi) PostUsersReportAsSpam(username string, v url.Values) (u User, err error) {
+	v = cleanValues(v)
+	v.Set("screen_name", username)
+	response_ch := make(chan response)
+	a.queryQueue <- query{a.baseUrl + "/users/report_spam.json", v, &u, _POST, response_ch}
+	return u, (<-response_ch).err
+}
+
+// PostUsersReportAsSpamById : Reports and Blocks a User by user_id
+// Reference : https://dev.twitter.com/rest/reference/post/users/report_spam
+// If you don't want to block the user you should add
+// v.Set("perform_block", "false")
+func (a TwitterApi) PostUsersReportAsSpamById(id int64, v url.Values) (u User, err error) {
+	v = cleanValues(v)
+	v.Set("user_id", strconv.FormatInt(id, 10))
+	response_ch := make(chan response)
+	a.queryQueue <- query{a.baseUrl + "/users/report_spam.json", v, &u, _POST, response_ch}
+	return u, (<-response_ch).err
+}
