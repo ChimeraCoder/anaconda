327ab838056c939c42db91f72f772973525057cd
Tom <tammo.mueller@gmail.com>
2017-09-03 11:27:59 -0400

    Add ability to add members in batch to list (#191)
    * Add ability to add members in batch to list
    
    * Add GetList functionality, implementing /lists/show.json

--- lists.go
+++ lists.go
@@ -3,6 +3,7 @@ package anaconda
 import (
 	"net/url"
 	"strconv"
+	"strings"
 )
 
 // CreateList implements /lists/create.json
@@ -29,6 +30,18 @@ func (a TwitterApi) AddUserToList(screenName string, listID int64, v url.Values)
 	return addUserToListResponse.Users, (<-response_ch).err
 }
 
+// AddMultipleUsersToList implements /lists/members/create_all.json
+func (a TwitterApi) AddMultipleUsersToList(screenNames []string, listID int64, v url.Values) (list List, err error) {
+	v = cleanValues(v)
+	v.Set("list_id", strconv.FormatInt(listID, 10))
+	v.Set("screen_name", strings.Join(screenNames, ","))
+
+	response_ch := make(chan response)
+	a.queryQueue <- query{a.baseUrl + "/lists/members/create_all.json", v, &list, _POST, response_ch}
+	r := <-response_ch
+	return list, r.err
+}
+
 // GetListsOwnedBy implements /lists/ownerships.json
 // screen_name, count, and cursor are all optional values
 func (a TwitterApi) GetListsOwnedBy(userID int64, v url.Values) (lists []List, err error) {
@@ -50,4 +63,14 @@ func (a TwitterApi) GetListTweets(listID int64, includeRTs bool, v url.Values) (
 	response_ch := make(chan response)
 	a.queryQueue <- query{a.baseUrl + "/lists/statuses.json", v, &tweets, _GET, response_ch}
 	return tweets, (<-response_ch).err
+}
+
+// GetList implements /lists/show.json
+func (a TwitterApi) GetList(listID int64, v url.Values) (list List, err error) {
+	v = cleanValues(v)
+	v.Set("list_id", strconv.FormatInt(listID, 10))
+
+	response_ch := make(chan response)
+	a.queryQueue <- query{a.baseUrl + "/lists/show.json", v, &list, _GET, response_ch}
+	return list, (<-response_ch).err
 }
