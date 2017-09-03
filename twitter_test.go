36425a02260b164bd7bca74cdc6e8f948d8e3dee
Aditya Mukerjee <dev@chimeracoder.net>
2017-09-03 12:03:07 -0400

    Run gofmt

--- twitter_test.go
+++ twitter_test.go
@@ -204,21 +204,21 @@ func Test_GetTweet(t *testing.T) {
 
 	// Check the entities
 	expectedEntities := anaconda.Entities{Hashtags: []struct {
-		Indices []int `json:"indices"`
+		Indices []int  `json:"indices"`
 		Text    string `json:"text"`
 	}{struct {
-		Indices []int `json:"indices"`
+		Indices []int  `json:"indices"`
 		Text    string `json:"text"`
-	}{Indices: []int{86, 93} , Text: "golang"}}, Urls: []struct {
-		Indices      []int `json:"indices"`
+	}{Indices: []int{86, 93}, Text: "golang"}}, Urls: []struct {
+		Indices      []int  `json:"indices"`
 		Url          string `json:"url"`
 		Display_url  string `json:"display_url"`
 		Expanded_url string `json:"expanded_url"`
 	}{}, User_mentions: []struct {
 		Name        string `json:"name"`
-		Indices     []int `json:"indices"`
+		Indices     []int  `json:"indices"`
 		Screen_name string `json:"screen_name"`
-		Id          int64 `json:"id"`
+		Id          int64  `json:"id"`
 		Id_str      string `json:"id_str"`
 	}{}, Media: []anaconda.EntityMedia{anaconda.EntityMedia{
 		Id:              303777106628841472,
