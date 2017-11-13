7edf4ed8e2cfb7d3f7f31dbb7bd6c2a743fc464a
Dylan Graham <dylan.graham@gmail.com>
2017-11-13 20:51:54 +1100

    Add GetUsersSuggestions

--- users.go
+++ users.go
@@ -55,6 +55,15 @@ func (a TwitterApi) GetUserSearch(searchTerm string, v url.Values) (u []User, er
 	return u, (<-response_ch).err
 }
 
+func (a TwitterApi) GetUsersSuggestions(v url.Values) (u []User, err error) {
+    v = cleanValues(v)
+    // Set other values before calling this method:
+    // page, count, include_entities
+    response_ch := make(chan response)
+    a.queryQueue <- query{a.baseUrl + "/users/suggestions.json", v, &u, _GET, response_ch}
+    return u, (<-response_ch).err
+}
+
 // PostUsersReportSpam : Reports and Blocks a User by screen_name
 // Reference : https://dev.twitter.com/rest/reference/post/users/report_spam
 // If you don't want to block the user you should add
